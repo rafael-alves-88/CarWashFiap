212edc54ca2dabb17cd5a726248bebeafdc32356
Rafael Alexandre Alves <raarafael.alves88@gmail.com>
2016-06-18 14:36:16 -0300

    Tela e Model de MeusAgendamentos

---
+++ CarWashFiap/CarWashFiap/Model/AgendamentoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWashFiap.Model
+{
+    public class AgendamentoModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Veiculo { get; set; }
+        public string Placa { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
